f3db70eb708e248d59e4dff0dd1c5a7c5320eaf5
Oscar Levin <oscar.levin@unco.edu>
2025-03-13 19:28:29 -0600

    set up fetching of sample book and article

--- scripts/fetch_projects.py
+++ scripts/fetch_projects.py
@@ -12,28 +12,31 @@ from fetch_core_commit import commit_data
 
 def main():
     last_core_commit = commit_data("pretext")["sha"]
-    core_zip_path = Path("pretext").resolve() / "resources" / "core.zip"
-    r = requests.get(f"https://github.com/pretextbook/pretext/archive/{last_core_commit}.zip")
-
-    with open(core_zip_path, "wb") as f:
-        f.write(r.content)
 
     with tempfile.TemporaryDirectory(prefix="ptxcli_") as tmpdirname:
+        core_zip_path = Path(tmpdirname) / "pretext.zip"
+        r = requests.get(f"https://github.com/pretextbook/pretext/archive/{last_core_commit}.zip")
+        print("Downloading core PreTeXtBook/pretext resources from GitHub...")
+        with open(core_zip_path, "wb") as f:
+            f.write(r.content)
         with zipfile.ZipFile(core_zip_path) as archive:
             archive.extractall(tmpdirname)
+            print("Extracted core PreTeXtBook/pretext resources from GitHub.")
 
             shutil.copytree(
                 Path(tmpdirname) / f"pretext-{last_core_commit}" / "examples" / "sample-book",
-                Path("projects").resolve(),
+                Path("projects").resolve() / "sample-book",
                 dirs_exist_ok=True,
             )
+            print("Copied sample-book to projects directory.")
             shutil.copytree(
                 Path(tmpdirname) / f"pretext-{last_core_commit}" / "examples" / "sample-article",
-                Path("projects").resolve(),
+                Path("projects").resolve() / "sample-article",
                 dirs_exist_ok=True,
             )
+            print("Copied sample-article to projects directory.")
 
-    print("Successfully updated core PreTeXtBook/pretext resources from GitHub.")
+    print("All done.")
 
 
 
